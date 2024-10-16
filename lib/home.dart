5284775f0966b51ad09d6a15465ba008b3c24b68
Sonu Kumar Gupta <sonu1505a@gmail.com>
2024-10-16 17:43:35 +0530

    "some"

---
+++ lib/home.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Container(
+          // child: Text("Bhavya Technology Pvt"),
+        )),
+    );
+  }
+}
